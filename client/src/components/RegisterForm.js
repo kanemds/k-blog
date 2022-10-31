842c6d4a600e9a79df0d7061280d32aa7072febd
Kanem <kanemdeng@email.com>
2022-10-31 03:12:29 -0700

    adding regex checking if register form is validate

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -2,9 +2,11 @@ import React, { useRef, useEffect, useState } from 'react'
 import { Paper, Box, Button, TextField, Typography } from '@mui/material'
 
 
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{4,24}$/
 // from 8 - 24 length, at least one: lower, uper, number and one of !@#$%  
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+// \w matches any words
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 const RegisterForm = () => {
 
@@ -13,22 +15,23 @@ const RegisterForm = () => {
 
   const [user, setUser] = useState('')
   const [validName, setValidName] = useState(false)
-  const [userFocus, setUserFocus] = useState(false)
+
+
+  const [email, setEmail] = useState('')
+  const [validEmail, setValidEmail] = useState(false)
 
   const [pwd, setPwd] = useState('')
   const [validPwd, setValidPwd] = useState(false)
-  const [pwdFocus, setPwdFocus] = useState(false)
+
 
   const [matchPwd, setMatchPwd] = useState('')
   const [validMatch, setValidMatch] = useState(false)
-  const [matchFocus, setMatchFocus] = useState(false)
+
 
   const [errorMessage, setErrorMessage] = useState('')
   const [success, setSuccess] = useState(false)
 
-  // useEffect(() => {
-  //   userRef.current.focus()
-  // }, [])
+
 
   useEffect(() => {
     const result = USER_REGEX.test(user)
@@ -38,24 +41,65 @@ const RegisterForm = () => {
   }, [user])
 
   useEffect(() => {
+    const result = EMAIL_REGEX.test(email)
+    console.log(result)
+    console.log(user)
+    setValidEmail(result)
+  }, [email])
+
+  useEffect(() => {
+    const result = matchPwd
+    console.log(result)
+    console.log(matchPwd)
+    const match = pwd === matchPwd
+  }, [matchPwd])
+
+  useEffect(() => {
     const result = PWD_REGEX.test(pwd)
     console.log(result)
     console.log(pwd)
-    const match = pwd === matchPwd
-    setValidMatch(match)
-  }, [pwd, matchPwd])
+    setValidPwd(result)
+  }, [pwd])
 
-  useEffect(() => {
-    setErrorMessage('')
-  }, [user, pwd, matchPwd])
+
+
 
   return (
     <Paper component="form" autoComplete="off" sx={{ display: 'flex', flexDirection: 'column', p: 2, maxWidth: "600px" }}>
       <Typography sx={{ display: 'flex', justifyContent: 'center', pt: 2, pb: 4 }} variant="h4" >Register</Typography>
-      <TextField type='text' label="User Name" variant="outlined" required helperText="Incorrect entry." />
-      <TextField type='email' label="E-mail" variant="outlined" required helperText="Incorrect entry." />
-      <TextField type='password' label="Password" variant="outlined" required helperText="Incorrect entry." />
-      <TextField type='password' label="Password Comfirm" variant="outlined" required helperText="Incorrect entry." />
+      <TextField sx={{ pb: 2 }} type='text' label="User Name" variant="outlined" required
+        onChange={e => setUser(e.target.value)}
+      />
+      {validName || user.length === 0 ? "" :
+        <Typography>User Name must be 4 to 24 characters</Typography>
+      }
+      <TextField sx={{ pb: 2 }} type='email' label="E-mail" variant="outlined" required
+        onChange={e => setEmail(e.target.value)}
+      />
+      {validEmail || email.length === 0 ? "" :
+        <Typography>This Email is invalid, Please try again</Typography>
+      }
+      <TextField sx={{ pb: 2 }} type='password' label="Password" variant="outlined" required
+        onChange={e => setPwd(e.target.value)}
+      />
+      {validPwd || pwd.length === 0 ? "" :
+        <Typography>
+          Password must be 8 to 24 characters and with the following characters: <br />
+          one capital letter
+          <br />
+          one number ( 0 - 9)
+          <br />
+          one special character:!,@,#,$,%
+        </Typography>
+      }
+      <TextField sx={{ pb: 2 }} type='password' label="Password Comfirm" variant="outlined" required
+        onChange={e => setMatchPwd(e.target.value)}
+      />
+      {matchPwd === pwd || matchPwd.length === 0 ? "" :
+        <Typography>
+          Does not match with password, Please try again
+        </Typography>
+      }
       <Button>Submit</Button>
       <Button>Back</Button>
     </Paper>
