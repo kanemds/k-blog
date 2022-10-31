3f11879a05ec4aedd8b7d863a3f43d4ac46fd63e
Kanem <kanemdeng@email.com>
2022-10-30 22:40:41 -0700

    adding baseQuery for giving token

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,1 +1,14 @@
 import { crateApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: '/',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`)
+    }
+    return headers
+  }
+})
