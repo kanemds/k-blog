f542752ea747105b1fd4d1f03c119c2bb01511ca
Kanem <kanemdeng@email.com>
2022-11-07 15:28:07 -0800

    setting routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,29 @@ import RegisterForm from './components/RegisterForm'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignInForm from './components/SignInForm'
 import Home from './pages/Home'
+import Welcome from './components/Welcome'
+import RequireAuth from './components/RequireAuth'
+import Layout from './components/Layout'
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='home' element={<Home />} />
-        <Route path='register' element={<RegisterForm />} />
-        <Route path='signin' element={<SignInForm />} />
+        <Route path='/' element={<Layout />}>
+          {/* Public */}
+          <Route index element={<Home />} />
+          <Route path='register' element={<RegisterForm />} />
+          <Route path='signin' element={<SignInForm />} />
+          {/* user only */}
+          <Route element={<RequireAuth />}>
+            <Route path='welcome' element={<Welcome />} />
+          </Route>
+
+        </Route>
       </Routes>
 
-    </Router>
+    </Router >
   )
 }
 export default App
